b8bedf50037ecf637b1e59d859634b8c87bc7a18
JMSal08 <jmsal08@gmail.com>
2025-10-24 01:18:49 +0800

    Added PlayAudio Function
    Added PlayAudio Function Block to load audio stream and play audio using the given file path or stream path

--- scripts/audio/audio_player.gd
+++ scripts/audio/audio_player.gd
@@ -7,3 +7,8 @@ func _on_ready() -> void:
 	await get_tree().create_timer(2).timeout
 	AudioPlay.stream = MainSoundTrack
 	AudioPlay.play()
+
+func PlayAudio(streamname: String) -> void:
+	var streamloader = load(streamname)
+	AudioPlay.stream = streamloader
+	AudioPlay.play(streamloader)
