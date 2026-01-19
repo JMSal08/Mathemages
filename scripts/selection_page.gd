5084873045af2fedbbe22326aa369bf86bcd332f
JMSal08 <jmsal08@gmail.com>
2026-01-19 23:04:15 +0800

    Added dummy code to selection page
    Clicking start game brings to agent selection page

--- scripts/selection_page.gd
+++ scripts/selection_page.gd
@@ -2,3 +2,11 @@ extends Control
 
 func _on_main_menu_btn_pressed() -> void:
 	TransitionScene.change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_import_btn_pressed() -> void:
+	pass
+
+
+func _on_start_game_btn_pressed() -> void:
+	TransitionScene.change_scene_to_file("res://scenes/game/agentSelection_page.tscn")
