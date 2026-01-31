ebca1cea3c2185960ab6306ba5daaa77e3395d63
JMSal08 <jmsal08@gmail.com>
2026-01-31 11:42:40 +0800

    Scrollable container in selection page

--- scripts/selection_page.gd
+++ scripts/selection_page.gd
@@ -4,7 +4,60 @@ const SELECTION_LIST_ITEM = preload("res://scenes/misc/selection_list_item.tscn"
 @onready var SelectedLabel = $SelectedLabel
 
 func _on_ready() -> void:
-	pass
+	var new_data_array: Array = gather_data()
+	
+	for i in ItemContainer.get_children():
+		i.queue_free()
+	
+	for item in new_data_array:
+		var new_list_item: SelectionItem = SELECTION_LIST_ITEM.instantiate()
+		new_list_item.rank = item.rank
+		new_list_item.title = item.title
+		new_list_item.file_path = item.file_path
+		new_list_item.subject = item.subject
+		
+		ItemContainer.add_child(new_list_item)
+	
+func gather_data() -> Array:
+	var data_array = [
+		{
+			"rank": 1,
+			"title" : "Evaulating Functions",
+			"file_path" :"res://package/default/EvaluateFunctions.json",
+			"subject" : "Mathematics" 
+		},
+		{
+			"rank": 2,
+			"title": "example",
+			"file_path": "example",
+			"subject": "example"
+		},
+		{
+			"rank": 2,
+			"title": "example",
+			"file_path": "example",
+			"subject": "example"
+		},
+		{
+			"rank": 3,
+			"title": "example",
+			"file_path": "example",
+			"subject": "example"
+		},
+		{
+			"rank": 4,
+			"title": "example",
+			"file_path": "example",
+			"subject": "example"
+		},
+		{
+			"rank": 5,
+			"title": "example",
+			"file_path": "example",
+			"subject": "example"
+		}
+	]
+	return data_array
 
 func _on_main_menu_btn_pressed() -> void:
 	TransitionScene.change_scene_to_file("res://scenes/main_menu.tscn")
