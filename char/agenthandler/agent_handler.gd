7757a24c4d5325733eb75c922f5203c66046448d
JMSal08 <jmsal08@gmail.com>
2026-02-06 20:29:15 +0800

    Explicit state to string get_agent_img

--- char/agenthandler/agent_handler.gd
+++ char/agenthandler/agent_handler.gd
@@ -14,7 +14,7 @@ var response_powerup = []
 var points
 var timer_multiplier = 1.0
 
-func get_agent_img(agent) -> Texture2D:
+func get_agent_img(agent: String) -> Texture2D:
 	var load_img = load("res://char/"+str(agent)+"/img/"+str(agent)+".png")
 	
 	return	load_img
