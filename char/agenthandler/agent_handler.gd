4f6f141604956a03ebd3b0ebd65b31e3ebbfba20
JMSal08 <jmsal08@gmail.com>
2026-02-20 18:03:35 +0800

    .

--- char/agenthandler/agent_handler.gd
+++ char/agenthandler/agent_handler.gd
@@ -2,6 +2,7 @@ extends Node
 
 #CHARACTER SETTINGS
 var AGENT = ""
+var path_to_file
 
 var response_correct = []
 var response_incorrect = []
@@ -20,3 +21,17 @@ func get_agent_img(agent: String) -> Texture2D:
 	
 	return	load_img
 	
+func load_agent(file_path: String):
+	file_path = path_to_file
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	if file == null:
+		TransitionScene.change_scene_to_file("res://scenes/main_menu.tscn")
+		push_error("Could not open file: %s" % file_path)
+		return
+	
+	var text = file.get_as_text()
+	var data = JSON.parse_string(text)
+	
+	if data == null:
+		push_error("Error parsing JSON File")
+		return
